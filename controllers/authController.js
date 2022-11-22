0fdb12351f441042f74bd8032163684b8d88da64
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-22 15:34:32 +0100

    feat: create login logic

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import User from '../models/User.js';
+import createSendToken from '../utils/createSendToken.js';
+import BadRequestError from '../errors/badRequest.js';
+import UnauthenticatedError from '../errors/unauthenticated.js';
+
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new BadRequestError('Please provide email and password'));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.comparePassword(password))) {
+    return next(new UnauthenticatedError('Incorrect email or password'));
+  }
+
+  createSendToken(user, StatusCodes.OK, req.res);
+});
