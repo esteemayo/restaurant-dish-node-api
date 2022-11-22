23c241a1a1ae2a117e14880719ce47b3bb429419
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-22 15:42:12 +0100

    feat: add changedPasswordAfter method

--- models/User.js
+++ models/User.js
@@ -44,6 +44,7 @@ const userSchema = new mongoose.Schema({
     },
     default: 'user',
   },
+  passwordChangedAt: Date,
 }, {
   timestamps: true,
 });
@@ -57,6 +58,17 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
