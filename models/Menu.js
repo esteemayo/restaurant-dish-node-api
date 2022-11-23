8287e28828d7546f91838a2f985e5b07263403d0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 21:30:05 +0100

    feat: handle slug

---
+++ models/Menu.js
@@ -0,0 +1,39 @@
+import slugify from 'slugify';
+import mongoose from 'mongoose';
+
+const menuSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A menu must have a name'],
+  },
+  slug: String,
+  price: {
+    type: Number,
+    required: [true, 'A menu must have a price'],
+  },
+  image: {
+    type: String,
+  },
+  ingredients: {
+    type: String,
+    required: [true, 'A menu must have ingredients'],
+  },
+}, {
+  timestamps: true,
+});
+
+menuSchema.pre('save', async function (next) {
+  if (!this.isModified('name')) return next();
+  this.slug = slugify(this.name, { lower: true });
+
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const menuWithSlug = await this.constructor.findOne({ slug: slugRegEx });
+
+  if (menuWithSlug.length) {
+    this.slug = `${this.slug}-${menuWithSlug.length + 1}`;
+  }
+});
+
+const Menu = mongoose.models.Menu || mongoose.model('Menu', menuSchema);
+
+export default Menu;
