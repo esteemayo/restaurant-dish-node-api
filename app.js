cf1080c7e05472c5ea28e366ff4e023f4e372a3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 21:15:26 +0100

    feat: add menus api route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 // requiring routes
 import authRoute from './routes/auth.js';
 import userRoute from './routes/users.js';
+import menuRoute from './routes/menus.js';
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
+app.use('/api/v1/menus', menuRoute);
 
 export default app;
