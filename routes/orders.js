80df2d94b4adfe32a98e837d2f67e8154522aff3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 21:40:07 +0100

    feat: add api endpoints

---
+++ routes/orders.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import authMiddleware from '../middlewares/authMiddleware.js';
+import * as orderController from '../controllers/orderController.js';
+
+const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router
+  .route('/')
+  .get(authMiddleware.restrictTo('admin'), orderController.getOrders)
+  .post(orderController.createOrder);
+
+router
+  .route('/:id')
+  .get(orderController.getOrder)
+  .patch(authMiddleware.restrictTo('admin'), orderController.updateOrder)
+  .delete(authMiddleware.restrictTo('admin'), orderController.deleteOrder);
+
+export default router;
