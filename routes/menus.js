c1e21f5d792ee021d7cfa8f4c6db8c1fbf46a641
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 18:42:05 +0100

    feat: add get all and post menu api endpoints

--- routes/menus.js
+++ routes/menus.js
@@ -1,5 +1,17 @@
 import express from 'express';
 
+import authMiddleware from '../middlewares/authMiddleware.js';
+import * as menuController from '../controllers/menuController.js';
+
 const router = express.Router();
+
+router
+  .route('/')
+  .get(menuController.getMenus)
+  .post(
+    authMiddleware.protect,
+    authMiddleware.restrictTo('admin'),
+    menuController.createMenu
+  );
 
 export default router;
