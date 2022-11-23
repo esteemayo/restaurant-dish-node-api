83f50ffa6ce29d33b36979d163b5b6fec401bdb0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 18:16:29 +0100

    feat: add review database schema

---
+++ models/Review.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+  remarks: {
+    type: String,
+    trim: true,
+    required: [true, 'A review must have remarks'],
+  },
+  rating: {
+    type: Number,
+    min: [1, 'Rating must not below 1.0'],
+    max: [5, 'Rating must not be above 5.0'],
+  },
+  menu: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Menu',
+    required: ['A review must belong to a menu'],
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A review must belong to a user'],
+  },
+}, {
+  timestamps: true,
+});
+
+const Review = mongoose.models.Review || mongoose.model('Review', reviewSchema);
+
+export default Review;
