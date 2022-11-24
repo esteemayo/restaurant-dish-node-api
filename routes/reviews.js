f0310fd3456975dafeafa7262bc0a0d8feab3b39
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-24 16:28:14 +0100

    feat: add all reviews api endpoints

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,18 @@
 import express from 'express';
 
+import * as reviewController from '../controllers/reviewController.js';
+
 const router = express.Router();
+
+router
+  .route('/')
+  .get(reviewController.getReviews)
+  .post(reviewController.createReview);
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
 
 export default router;
