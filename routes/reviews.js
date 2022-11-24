c061911d056d0f56c03069ac5dbc01e448f2f359
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-24 16:40:42 +0100

    feat: import authMiddleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import authMiddiware from '../middlewares/authMiddleware.js';
 import * as reviewController from '../controllers/reviewController.js';
 
 const router = express.Router();
